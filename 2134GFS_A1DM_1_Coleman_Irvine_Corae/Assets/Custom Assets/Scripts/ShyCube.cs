7b01f189a64205453cf4493d7faaa6b4fabccb9d
teagani <teaganirvine2426@gmail.com>
2023-07-25 21:12:57 +1000

    Finished Orbs

--- 2134GFS_A1DM_1_Coleman_Irvine_Corae/Assets/Custom Assets/Scripts/ShyCube.cs
+++ 2134GFS_A1DM_1_Coleman_Irvine_Corae/Assets/Custom Assets/Scripts/ShyCube.cs
@@ -7,7 +7,7 @@ public class ShyCube : MonoBehaviour
 
 	public Transform location;
 
-	public Transform cube, cube2, cube3, cube4, cube5;
+	public Transform cube, cube2, cube3, cube4, cube5, cube6, cube7, cube8, cube9, cube10, cube11, cube12, cube13, cube14, cube15, cube16, cube17, cube18, cube19, cube20, cube21, cube22, cube23, cube24, cube25, cube26, cube27;
 
 	public float max;
 
@@ -32,6 +32,28 @@ public class ShyCube : MonoBehaviour
 			cube3.localScale = new Vector3(scale, scale, scale);
 			cube4.localScale = new Vector3(scale, scale, scale);
 			cube5.localScale = new Vector3(scale, scale, scale);
+			cube6.localScale = new Vector3(scale, scale, scale);
+			cube7.localScale = new Vector3(scale, scale, scale);
+			cube8.localScale = new Vector3(scale, scale, scale);
+			cube9.localScale = new Vector3(scale, scale, scale);
+			cube10.localScale = new Vector3(scale, scale, scale);
+			cube11.localScale = new Vector3(scale, scale, scale);
+			cube12.localScale = new Vector3(scale, scale, scale);
+			cube13.localScale = new Vector3(scale, scale, scale);
+			cube14.localScale = new Vector3(scale, scale, scale);
+			cube15.localScale = new Vector3(scale, scale, scale);
+			cube16.localScale = new Vector3(scale, scale, scale);
+			cube17.localScale = new Vector3(scale, scale, scale);
+			cube18.localScale = new Vector3(scale, scale, scale);
+			cube19.localScale = new Vector3(scale, scale, scale);
+			cube20.localScale = new Vector3(scale, scale, scale);
+			cube21.localScale = new Vector3(scale, scale, scale);
+			cube22.localScale = new Vector3(scale, scale, scale);
+			cube23.localScale = new Vector3(scale, scale, scale);
+			cube24.localScale = new Vector3(scale, scale, scale);
+			cube25.localScale = new Vector3(scale, scale, scale);
+			cube26.localScale = new Vector3(scale, scale, scale);
+			cube27.localScale = new Vector3(scale, scale, scale);
 		}
 	}
 }
